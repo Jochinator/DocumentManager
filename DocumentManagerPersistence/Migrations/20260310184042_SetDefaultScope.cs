1a8f43cc00e3c38c03198d79b1d725b2622f2437
Jochinator <18726483+Jochinator@users.noreply.github.com>
2026-03-10 19:44:26 +0100

    Add Migration to set default scope

---
+++ DocumentManagerPersistence/Migrations/20260310184042_SetDefaultScope.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentManagerPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultScope : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Metadatas",
+                keyColumn: "Scope",
+                keyValue: "",
+                column: "Scope",
+                value: "default");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
