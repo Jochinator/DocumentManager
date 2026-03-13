cb4759dc6e659c56d95100e1ffe42dd6a3f8c9d4
Jochinator <18726483+Jochinator@users.noreply.github.com>
2026-03-13 13:58:04 +0100

    Add Unique Constraint to Tags

--- DocumentManagerPersistence/DocumentContext.cs
+++ DocumentManagerPersistence/DocumentContext.cs
@@ -11,6 +11,14 @@ public class DocumentContext: DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
         optionsBuilder.UseSqlite($"Data Source={DbPath}");
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TagDao>()
+            .HasIndex(t => t.Value)
+            .IsUnique();
+    }
+
     public DbSet<DocumentMetadataDao> Metadatas { get; set; }
     public DbSet<TagDao> Tags { get; set; }
     public DbSet<DataMigrationDao> DataMigrations { get; set; }
---
+++ DocumentManagerPersistence/Migrations/20260313125733_AddUniqueConstraintToTags.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentManagerPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Value",
+                table: "Tags",
+                column: "Value",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_Value",
+                table: "Tags");
+        }
+    }
+}
