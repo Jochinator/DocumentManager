9201840338f4dace42c9e26c55aabe89a6b7ffa0
Jochinator <18726483+Jochinator@users.noreply.github.com>
2026-03-13 21:32:10 +0100

    Add ContactController.cs

---
+++ DocumentManagerApi/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using DocumentManager;
+using DocumentManagerPersistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentManagerApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContactController : ControllerBase
+{
+    private readonly ContactRepository _repository;
+
+    public ContactController(ContactRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public IEnumerable<ContactDto> GetContacts()
+    {
+        return _repository.GetContacts();
+    }
+
+    [HttpPut("{id}")]
+    public ContactDto UpdateContact([FromRoute] Guid id, [FromBody] ContactDto contact)
+    {
+        contact.Id = id;
+        return _repository.UpdateContact(contact);
+    }
+
+    [HttpDelete("{id}")]
+    public void DeleteContact([FromRoute] Guid id)
+    {
+        _repository.DeleteContact(id);
+    }
+}
